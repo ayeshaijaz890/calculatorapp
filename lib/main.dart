9e8d24ac6ca0bb4e57eab114761de4440bc50939
ayeshaijaz890 <ayeshaahmed07@gmail.com>
2021-06-28 04:59:39 -0700

    now start

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+
+// import 'package:calculatorapp/home.dart';
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Calculator(),
+      ),
+      
+    );
+  }
+}
+
+
+    
